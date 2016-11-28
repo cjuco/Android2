72e3ca503c6f18793ecb043a93581377206cb77b
Cju <cjuco@naver.com>
2016-11-28 22:15:00 +0900

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cju.gitconnecttest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //깃허브에 올라가는거 테스트해보기
+    }
+}
